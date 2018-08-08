1ac8d79ad9dae69c9e2c3bf67c923011d9cda683
inked-monkey <pablo.vicencio@correoaiep.cl>
2018-08-08 18:00:44 -0400

    Mis Datos
    codificacion de estilo y carga de informacion mis datos

--- clases/Funciones.php
+++ clases/Funciones.php
@@ -6,7 +6,37 @@ require_once '../recursos/db/db.php';
 class Funciones 
 {
 
+    /*///////////////////////////////////////
+    Cargar datos de usuario
+    //////////////////////////////////////*/
+        public function cargar_datos_usu($id_usu){
 
+            try{
+                
+                
+                $pdo = AccesoDB::getCon();
+
+
+        
+                        $sql = "select concat(a.NOM1_USU,' ',a.NOM2_USU) nom, concat(a.APEPAT_USU,' ',a.APEPAT_USU) ape, a.RUT_USU rut,
+                                a.MAIL_USU mail,c.desc_item perfil, a.FEC_CRE_USU fec, b.DESC_ITEM cargo, if(a.VIG_USU=1,'Vigente','No Vigente') vig, a.NICK_USU nick
+                                from usuarios a, tab_param b, tab_param c
+                                where a.CARGO_USU = b.COD_ITEM and b.COD_GRUPO = 2 and b.VIG_ITEM = 1 
+                                and a.ID_PERFIL = c.COD_ITEM and c.COD_GRUPO = 1 and c.VIG_ITEM = 1
+                                and a.ID_USU = :id_usu";
+                
+
+                $stmt = $pdo->prepare($sql);
+                $stmt->bindParam(":id_usu", $id_usu, PDO::PARAM_INT);
+                $stmt->execute();
+
+                $response = $stmt->fetchAll();
+                return $response;
+
+            } catch (Exception $e) {
+                echo"<script type=\"text/javascript\">alert('Error, comuniquese con el administrador".  $e->getMessage()." '); window.location='../paginas_usu/clientes.php';</script>";
+            }
+        }
   
 
 
--- paginas_fa/datos_pers.php
+++ paginas_fa/datos_pers.php
@@ -7,7 +7,13 @@ if( isset($_SESSION['id_fac']) and ($_SESSION['perfil_fac'] <> 0) ){
   else{
     //Si no lo redirige a la pagina index para que inicie la sesion 
     header("location: ../index.html");
-  }      
+  }   
+
+   require_once '../clases/Funciones.php';
+  
+  
+
+  $fun = new Funciones();    
 ?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -18,6 +24,18 @@ if( isset($_SESSION['id_fac']) and ($_SESSION['perfil_fac'] <> 0) ){
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+  <style type="text/css">
+    #main{
+      background: url('../recursos/img/logo/bg.jpg');
+      background-repeat: no-repeat;
+      background-attachment: fixed;
+      background-position: center; 
+      border-radius: 25px;
+      padding-top: 5px;
+    }
+  </style>
 
  
 </head>
@@ -28,8 +46,8 @@ if( isset($_SESSION['id_fac']) and ($_SESSION['perfil_fac'] <> 0) ){
       <nav class="navbar navbar-expand-sm ">
               <img class="img-fluid" src="../recursos/img/logo/logo_fa.png" alt="Viracocha" width="150" height="30">
               <ul class="navbar-nav ml-auto" >
-               <li class="nav-item">Bienvenido: <?php echo $_SESSION['nom_fac']; ?></li>
-                <li class="nav-item"><a class="nav-link" href="../controles/logout.php" onclick="return confirm('¿Deseas finalizar sesión?');">Cerrar Sesión</a></li>
+               <li class="nav-item">Bienvenido: <br><b><?php echo $_SESSION['nom_fac']; ?></b> </li>
+                <li class="nav-item"><a class="nav-link" href="../controles/logout.php" onclick="return confirm('¿Deseas finalizar sesión?');"><i style="font-size:24px" class="fa">&#xf08b;</i>Cerrar Sesión</a></li>
               </ul>
       </nav>
 
@@ -65,7 +83,67 @@ if( isset($_SESSION['id_fac']) and ($_SESSION['perfil_fac'] <> 0) ){
                 <li class="nav-item"><a class="nav-link" href="#">Informes</a></li>
               </ul>
       </nav>
-  <div class="container" >
+
+<div class="container" id="main">
+  <div class="row">
+  <div class="col-12">
+    <h3>Mis Datos</h3>
+    <hr>
   </div>
+  </div>
+                  <?php
+                  $re = $fun->cargar_datos_usu($id);   
+                  foreach($re as $row)      
+                      {
+                         
+                      }    
+                  ?>  
+  <div class="row" >
+  <div class="col-6">
+
+          <div class="form-group">
+            <label for="nom">Nombres:</label>
+            <input type="text" class="form-control" id="nom_usu" name="nom_usu"  value="<?php echo $row['nom'] ?>" readonly>
+          </div>
+          <div class="form-group">
+              <label for="ape">Apellidos</label>
+              <input type="text" class="form-control" name="ape_usu" id="ape_usu" value="<?php echo $row['ape'] ?>" readonly>
+            </div>
+          <div class="form-group">
+             <label for="rut">Rut:</label>
+             <input type="text"  class="form-control" id="rut_usu" name="rut_usu" value="<?php echo $row['rut'] ?>"  readonly>
+          </div>
+          <div class="form-group">
+             <label for="mail">Mail:</label>
+             <input type="text" class="form-control" id="mail_usu" name="mail_usu" value="<?php echo $row['mail'] ?>"  readonly>
+          </div>
+           <div class="form-group">
+             <label for="perfil">Perfil de Sistema:</label>
+             <input type="text" class="form-control" id="perfil_usu" name="perfil_usu" value="<?php echo $row['perfil'] ?>"  readonly>
+          </div>
+  </div>
+  <div class="col-6">
+        <div class="form-group">
+            <label for="fec">Fecha de Creación:</label>
+            <input type="text" class="form-control" id="fec_usu" name="fec_usu"  value="<?php echo date('d-m-Y', strtotime($row['fec'])) ?>" readonly>
+          </div>
+          <div class="form-group">
+              <label for="cargo">Cargo</label>
+              <input type="text" class="form-control" name="cargo_usu" id="cargo_usu" value="<?php echo $row['cargo'] ?>" readonly>
+            </div>
+          <div class="form-group">
+             <label for="vig">Vigencia:</label>
+             <input type="text"  class="form-control" id="vig_usu" name="vig_usu" value="<?php echo $row['vig'] ?>"  readonly>
+          </div>
+          <div class="form-group">
+             <label for="nick">Nickname:</label>
+             <input type="text" class="form-control" id="nick_usu" name="nick_usu" value="<?php echo $row['nick'] ?>"  readonly>
+          </div>
+  </div>
+  </div>
+
+
+
+</div>
 </body>
 </html>
